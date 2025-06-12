fbd3f3b884f2bff85b0cf95f92d07199918d6c7f
Wirda0665 <wirda.hajiza2005@gmail.com>
2025-06-12 11:30:19 +0800

    initial commit

--- DataDiri.java
+++ DataDiri.java
@@ -4,6 +4,7 @@ public class DataDiri {
     String nama, npm;
     int kelas;
     
+    //membuat method constructor
     public DataDiri(String nama, int kelas, String npm) {
         this.nama = nama;
         this.npm = npm;
