2ecc322c5aa33185aea31a0e6c91bcc6f731a567
Wirda0665 <wirda.hajiza2005@gmail.com>
2025-06-12 11:29:22 +0800

    Initial commit

---
+++ DataDiri.java
@@ -0,0 +1,15 @@
+package latihan2;
+
+public class DataDiri {
+    String nama, npm;
+    int kelas;
+    
+    public DataDiri(String nama, int kelas, String npm) {
+        this.nama = nama;
+        this.npm = npm;
+        this.kelas = kelas;
+    }
+    
+    
+    
+}
